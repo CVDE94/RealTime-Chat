cd76451240610532f2efae837188b431261bbcb3
MichiDevelop <michidevelop@gmail.com>
2023-01-22 23:00:33 -0600

    Seccion 7: Clase 62.Pull to refresh

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:real_time_chat/models/usuario.dart';
 
 class UsuariosPage extends StatefulWidget {
@@ -10,6 +11,9 @@ class UsuariosPage extends StatefulWidget {
 }
 
 class _UsuariosPageState extends State<UsuariosPage> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
   final usuarios = [
     Usuario(uuid: '01', usuario: 'Michi', email: 'test1@tes.com', online: true),
     Usuario(uuid: '02', usuario: 'Mali', email: 'test2@tes.com', online: false),
@@ -19,44 +23,67 @@ class _UsuariosPageState extends State<UsuariosPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Mi Usuaro',
-            style: GoogleFonts.roboto(),
-          ),
-          centerTitle: true,
-          elevation: 1,
-          leading: IconButton(
-            icon: const Icon(Icons.exit_to_app_outlined),
-            onPressed: () {},
-          ),
-          actions: [
-            Container(
-              margin: const EdgeInsets.only(right: 10),
-              child:
-                  const Icon(Icons.check_circle_outline, color: Colors.green),
-              //child: const Icon(Icons.offline_bolt_outlined, color: Colors.red),
-            ),
-          ],
+      appBar: AppBar(
+        title: Text(
+          'Mi Usuaro',
+          style: GoogleFonts.roboto(),
         ),
-        body: ListView.separated(
-          physics: BouncingScrollPhysics(),
-          itemBuilder: (_, i) => ListTile(
-            title: Text(usuarios[i].usuario),
-            leading: CircleAvatar(
-              backgroundColor: Colors.indigo,
-              child: Text(usuarios[i].usuario.substring(0, 2)),
-            ),
-            trailing: Container(
-              width: 10,
-              height: 10,
-              decoration: BoxDecoration(
-                  color: usuarios[i].online ? Colors.green : Colors.red,
-                  borderRadius: BorderRadius.circular(100)),
-            ),
+        centerTitle: true,
+        elevation: 1,
+        leading: IconButton(
+          icon: const Icon(Icons.exit_to_app_outlined),
+          onPressed: () {},
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: const Icon(Icons.check_circle_outline, color: Colors.green),
+            //child: const Icon(Icons.offline_bolt_outlined, color: Colors.red),
           ),
-          separatorBuilder: (_, i) => const Divider(),
-          itemCount: usuarios.length,
-        ));
+        ],
+      ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: true,
+        onRefresh: _cargarUsuarios,
+        header: const WaterDropHeader(
+          complete: Icon(Icons.check, color: Colors.indigo),
+          waterDropColor: Colors.indigo,
+        ),
+        child: _listViewUsuarios(),
+      ),
+    );
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, i) => _usuarioListTitle(usuarios[i]),
+      separatorBuilder: (_, i) => const Divider(),
+      itemCount: usuarios.length,
+    );
+  }
+
+  ListTile _usuarioListTitle(Usuario usuario) {
+    return ListTile(
+      title: Text(usuario.usuario),
+      subtitle: Text(usuario.email),
+      leading: CircleAvatar(
+        backgroundColor: Colors.indigo,
+        child: Text(usuario.usuario.substring(0, 2)),
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            color: usuario.online ? Colors.green : Colors.red,
+            borderRadius: BorderRadius.circular(100)),
+      ),
+    );
+  }
+
+  _cargarUsuarios() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    _refreshController.refreshCompleted();
   }
 }
