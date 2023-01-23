85c05c89b52f21480916a0805707dde2980c7250
MichiDevelop <michidevelop@gmail.com>
2023-01-22 18:20:13 -0600

    Seccion 6: Clase 50 (Inicio de aplicacion)

---
+++ lib/pages/pages.dart
@@ -0,0 +1,5 @@
+export 'package:real_time_chat/pages/chat_page.dart';
+export 'package:real_time_chat/pages/loading_page.dart';
+export 'package:real_time_chat/pages/login_page.dart';
+export 'package:real_time_chat/pages/register_page.dart';
+export 'package:real_time_chat/pages/usuarios_page.dart';
